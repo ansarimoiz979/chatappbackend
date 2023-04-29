132d5330fbd5b19cf5e5304f5486e619cd176ac9
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-04-29 23:06:36 +0530

    formating of document, correction in user routefile

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,45 +1,42 @@
 const router = require("express").Router();
 const User = require("../model/user.model")
-router.post("/register",async (req , res)=>{
+router.post("/register", async (req, res) => {
     try {
-        const { name , email , password} = req.body
-    const user = new User({ name : name,email : email, password : password})    
-    let userdata = await user.save()
-           res.json({  data : userdata })
-    }catch(err)
-    {
-        console.log("err",err)
-        res.json({  err : err })
-        
+        const { name, email, password } = req.body
+        const user = new User({ name: name, email: email, password: password })
+        let userdata = await user.save()
+        res.json({ data: userdata })
+    } catch (err) {
+        console.log("err", err)
+        res.json({ err: err })
+
     }
 })
 
-router.post("/loginToPortal",async (req, res)=>{
-    try{
-        const { email , password } = req.body
-        let userInfo = await User.findOne({ email : email , password : password })
-        if(!userInfo)
-        {
-            res.json({ message : "USER_DOES_NOT_EXIST", data : userInfo })
-        }else{
-            res.json({ message : "USER_EXIST", data : userInfo , status : 200})
+router.post("/loginToPortal", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        let userInfo = await User.findOne({ email: email, password: password })
+        if (!userInfo) {
+            res.json({ message: "USER_DOES_NOT_EXIST", data: userInfo })
+        } else {
+            res.json({ message: "USER_EXIST", data: userInfo, status: 200 })
         }
-    }catch(err){
-        console.log("error in logintoportal",err);
-        res.json({ err : err })
+    } catch (err) {
+        console.log("error in logintoportal", err);
+        res.json({ err: err })
     }
 })
 
 
-router.get("/userList",async (req , res)=>{
+router.get("/userList", async (req, res) => {
     try {
         let userdata = await User.find()
-        res.status(200).json({  data : userdata })
-    }catch(err)
-    {
-        console.log("err",err)
-        res.status(500).json({  err : err })
-        
+        res.status(200).json({ data: userdata })
+    } catch (err) {
+        console.log("err", err)
+        res.status(500).json({ err: err })
+
     }
 })
 
